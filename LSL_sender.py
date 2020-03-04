16846912909407c8b2362b5aade5f948686e7ae3
adeena210 <57496912+adeena210@users.noreply.github.com>
2020-03-04 08:54:04 -0500

    Update LSL_sender.py

--- LSL_sender.py
+++ LSL_sender.py
@@ -1,6 +1,11 @@
 import time
 from random import random as rand
 from pylsl import StreamInfo, StreamOutlet
+from muselsl.constants import MUSE_SCAN_TIMEOUT, AUTO_DISCONNECT_DELAY,  \
+    MUSE_NB_EEG_CHANNELS, MUSE_SAMPLING_EEG_RATE, LSL_EEG_CHUNK,  \
+    MUSE_NB_PPG_CHANNELS, MUSE_SAMPLING_PPG_RATE, LSL_PPG_CHUNK, \
+    MUSE_NB_ACC_CHANNELS, MUSE_SAMPLING_ACC_RATE, LSL_ACC_CHUNK, \
+    MUSE_NB_GYRO_CHANNELS, MUSE_SAMPLING_GYRO_RATE, LSL_GYRO_CHUNK
 
 info = StreamInfo('BioSemi', 'EEG', 8, 100, 'float32', 'myuid34234')
 outlet = StreamOutlet(info)
